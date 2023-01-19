dd740ad367e8e8d24852702eca25a634538bef92
FelixKretke <f.kretke@gmail.com>
2023-01-19 20:10:55 +0100

    add counting sort

--- v1/main_v1.cpp
+++ v1/main_v1.cpp
@@ -1,33 +1,26 @@
 #include "../definitions.h"
 #include "../utils.h"
 #include "quick_sort_v1.h"
-#include "merge_sort_v1.h"
 #include "insertion_sort_v1.h"
-#include "bubble_sort_v1.h"
+#include "counting_sort_v1.h"
 
 int main() {
     std::vector<sort::AlgorithmInformation> algorithm_information{
-            {sort::quicksort<sort::Sortable::iterator>,      "quick_sort",
+            {sort::quicksort<sort::Sortable::iterator>,                "quick_sort",
                     {
                             {sort::BEST_CASE, sort::quicksort_best_case_generator},
                             {sort::WORST_CASE, sort::quicksort_worst_case_generator}
                     }
             },
-            {sort::merge_sort<sort::Sortable::iterator>,     "merge_sort",
-                    {}
-            },
-            {sort::insertion_sort<sort::Sortable::iterator>, "insertion_sort",
+            {sort::insertion_sort<sort::Sortable::iterator>,           "insertion_sort",
                     {
                             {sort::BEST_CASE, sort::insertion_sort_best_case_generator},
                             {sort::WORST_CASE, sort::insertion_sort_worst_case_generator}
                     }
             },
             {
-             sort::bubble_sort<sort::Sortable::iterator>,    "bubble_sort",
-                    {
-                            {sort::BEST_CASE, sort::bubble_sort_best_case_generator},
-                            {sort::WORST_CASE, sort::bubble_sort_worst_case_generator}
-                    }
+             sort::counting_sort<sort::Sortable::iterator, MAX_VALUE>, "counting_sort",
+                    {}
             }
     };
 
